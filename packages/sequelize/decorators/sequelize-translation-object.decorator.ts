fa306229f6e5cf71ad649f10fa863f8139a17725
Jtplouffe <jtplouffe@gmail.com>
2022-03-28 15:43:14 -0400

    Renamed decorator

--- packages/sequelize/decorators/sequelize-translation-object.decorator.ts
+++
@@ -1,7 +0,0 @@
-export const SEQUELIZE_TRANSLATION_OBJECT_METADATA_KEY = "sequelize_translation_object_metadata_key";
-
-export const SequelizeTranslationObject = (): PropertyDecorator => {
-    return (target: any, propertyKey: string | symbol) => {
-        Reflect.defineMetadata(SEQUELIZE_TRANSLATION_OBJECT_METADATA_KEY, true, target, propertyKey);
-    };
-}
