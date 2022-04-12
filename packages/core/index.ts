1d1a7dece209b108c72b682341559e94ce57a5a7
Jtplouffe <jtplouffe@gmail.com>
2022-04-12 14:49:41 -0400

    Removed unused model

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -4,4 +4,3 @@ export * from "./interfaces/nestjs-rosetta.options";
 export * from "./interfaces/nestjs-rosetta-interceptor.config";
 export * from "./interceptors/nestjs-rosetta.interceptor";
 export * from "./models/translation-object.model";
-export * from "./models/transformed-value.model";
--- packages/core/processors/translation-object.processor.ts
+++ packages/core/processors/translation-object.processor.ts
@@ -1,7 +1,3 @@
-import { Injectable } from "@nestjs/common";
-import { TranslationObject } from "../models/translation-object.model";
-import { TransformedValue } from "../models/transformed-value.model";
-
 export interface TranslationObjectProcessor {
     canProcess(value: any): boolean;
 
