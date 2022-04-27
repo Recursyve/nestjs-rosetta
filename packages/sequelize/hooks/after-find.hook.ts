e8d9e94b827531edbaa11da89f011a983c05750f
Jtplouffe <jtplouffe@gmail.com>
2022-04-27 15:01:21 -0400

    Fix

--- packages/sequelize/hooks/after-find.hook.ts
+++ packages/sequelize/hooks/after-find.hook.ts
@@ -13,6 +13,9 @@ export function NestjsRosettaSequelizeAfterFind(instanceOrInstances: Model | Mod
         for (const key of Object.keys(instance["dataValues"])) {
             if (Reflect.getMetadata(TRANSLATION_COLUMN_METADATA_KEY, Object.getPrototypeOf(instance), key)) {
                 instance["dataValues"][key] = new TranslationObject(instance["dataValues"][key]);
+            } else if (instance["dataValues"][key]?.["dataValues"]) {
+                // Run the hook on nested models
+                NestjsRosettaSequelizeAfterFind(instance["dataValues"][key]);
             }
         }
     }
