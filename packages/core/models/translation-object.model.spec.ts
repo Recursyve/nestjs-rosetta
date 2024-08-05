aa970e44d291354425a0e01333f4f4e1cbd48247
Nycola Plaisance <nycola.plaisance@hotmail.com>
2024-08-05 17:11:18 -0400

    added isTranslatedIn tests

--- packages/core/models/translation-object.model.spec.ts
+++ packages/core/models/translation-object.model.spec.ts
@@ -13,4 +13,30 @@ describe("TranslationObject", () => {
         expect(obj.getOrFirstIfNull("fr")).toEqual("Test!");
         expect(obj.getOrFirstIfNull("en")).toEqual(null);
     });
+
+    describe("isTranslatedId", () => {
+        it("should return true if the translation object is translated in all the languages passed in", () => {
+            const translationObject = new TranslationObject({ fr: "oui", en: "yes" });
+
+            expect(translationObject.isTranslatedIn(["fr", "en"])).toEqual(true);
+        })
+
+        it("should return true if the translation object is translated in all the languages passed in and much more", () => {
+            const translationObject = new TranslationObject({ fr: "oui", en: "yes", es: "si" });
+
+            expect(translationObject.isTranslatedIn(["fr", "en"])).toEqual(true);
+        })
+
+        it("should return false if the translation object is translated in some of the languages passed", () => {
+            const translationObject = new TranslationObject({ fr: "oui", en: "yes" });
+
+            expect(translationObject.isTranslatedIn(["fr", "es"])).toEqual(false);
+        })
+
+        it("should return false if the translation object is not translated in any of the languages passed", () => {
+            const translationObject = new TranslationObject({ es: "si" });
+
+            expect(translationObject.isTranslatedIn(["fr", "en"])).toEqual(false);
+        })
+    });
 });
